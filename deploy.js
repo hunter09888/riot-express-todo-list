4e8eb84d09b0a4cd06515183a4624356a91a123e
Jackson <hunterneal56@yahoo.com>
2020-10-04 21:03:25 -0500

    deploy

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: 'ec2-34-224-27-235.compute-1.amazonaws.com',
       username: 'ubuntu',
-      privateKey: 'hs-key.ppk'
+      privateKey: 'hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
